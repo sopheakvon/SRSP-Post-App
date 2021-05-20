5e3e36eb4d0f2524aaf7fa2f9eeaf7626427096e
saravey2021 <sara.vey@student.passerellesnumeriques.org>
2021-05-20 18:44:43 +0700

    uouo

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,4 @@
-function displayUser(response){
-    // console.log(response.data);
-    let users = response.data;
-    let userList = document.querySelector('.user-list');
-    let ul = document.querySelector('ul');
-    if (ul !== null){
-        ul.remove();
-    }
-
-    const newUl = document.createElement('ul');
-    for (let user of users) {
-        const li = document.createElement('li');
-        li.textContent = user.id + " " +user.username + " " + user.password;
-        newUl.appendChild(li);
-        userList.appendChild(newUl);
-    }
-}
-
-function save(event) {
-    const username = document.querySelector('#username');
-    const password = document.querySelector('#password');
-
-    let user = {username: username,  password: password};
-    const url ="http://localhost:5000/api/users";
-    axios
-    .post(url, user)
-    .then(displayUser);
-}
-
-// function loadData() {
-//     const url="http://localhost:5000/api/users";
-//     axios
-//     .get(url)
-//     .then(displayUser)
-// }
 
 
-function deleteUser () {
 
-}
 
-function updateUser () {
-    
-}
-const saveUser = document.querySelector('#save');
-saveUser.addEventListener('click', save);
-
-// loadData();
