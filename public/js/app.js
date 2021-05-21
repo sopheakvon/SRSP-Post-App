15b2f048f67397a2602a203a65651c4c6c1d817a
saravey2021 <sara.vey@student.passerellesnumeriques.org>
2021-05-21 08:49:55 +0700

    m

--- public/js/app.js
+++ public/js/app.js
@@ -1,72 +1,39 @@
-function displayUser(response){
-    // console.log(response.data);
-    let users = response.data;
-    let userList = document.querySelector('.message');
-    let ul = document.querySelector('ul');
-    if (ul !== null){
-        ul.remove();
-    }
-    
-    const newUl = document.createElement('ul');
-    for(let comment of users){
-        // const ul = document.querySelector('ul');
-        const li = document.createElement('li');
-        li.textContent = comment.username + " : " + comment.message;
-        ul.appendChild(li);
-        userList.appendChild(ul);
-    const comments = response.data;
+function displayComment (response){
+    let comments = response.data;
+    const ul = document.querySelector('ul');
+
     for(let comment of comments){
+
         const sms = document.createElement('div');
         sms.classList.add('alert');
         sms.classList.add('alert-info');
 
         const userName = document.createElement('strong');
         const messages = document.createElement('span');
+
         userName.textContent = comment.username + " : ";
         messages.textContent = comment.message;
+
         sms.appendChild(userName);
         sms.appendChild(messages);
         cardComment.appendChild(sms);
-        
     }
 }
 
-function save(){
-    const comments = document.querySelector('#comments').value;
-    let user = {comments: comments};
-    const url = "http://localhost:5000/comments";
+function save(event){
+    const item = document.querySelector('.form-control').value;
+    // let newItem = {username: parseInt(id), message: item};
+    const url ="http://localhost:5000/comments";
     axios
-    .get(url, user)
-    .then(displayUser);
-}
-
-function loadData() {
-    const url="http://localhost:5000/comments";
-    axios
+    // .get(url, newItem)
     .get(url)
-    .then(displayUser)
+    .then(displayComment)
 }
-
-function deleteUser () {
-
-}
-
-function updateUser () {
-    
-}
-// const btnSend = document.querySelector('button');
-const saveUser = document.querySelector('#send-comment');
-saveUser.addEventListener('click', save);
-save();
-
-
-
-
-
+// const url ="http://localhost:5000/comments";
+// axios
+// .get(url)
+// .then(displayComment)
 
 const btnSend = document.querySelector('button');
-
 const cardComment = document.querySelector('.card-comment');
-
 save();
-
--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-
-
-app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
+app.listen(port, () => console.log("server starting ..."));
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -13,71 +11,17 @@ app.use(express.static('public'));
 let comments  = [
     {id: 1, username: "Sara", message: "Hello!! My name is Sara"},
     {id: 2, username: "Sopheak", message: "Hello!! My name is Sopheak"},
-    {id: 2, username: "Rady", message: "Hello!! My name is Rady"},
+    // {id: 2, username: "Rady", message: "Hello!! My name is Rady"},
 ];
 
 // GET Method
 app.get('/comments', (req, res) =>{
+    //
+    let newItem = req.body
+    comments.push(newItem);
     res.send(comments)
 });
 
-
-// app.post('/api/users', (req, res) => {
-//     console.log(req.body);
-//     if (!req.body.password) {
-//         res.status(400)
-//         res.send({error: "password Required"})
-//     }
-//     let user = {
-//         id: users.length +1,
-//         username: req.body.username,
-//         password: req.body.password
-//     }
-//     users.push(user);
-//     res.send(users);
-// });
-
-// app.delete('/api/users/:id', (req, res) => {
-//     let id = req.params.id;
-//     // console.log(id);
-//     let index = -1;
-//     for (let user of users) {
-//         if (user.id === parseInt(id)){
-//             index = user.id -1;
-//         }
-//     }
-//     if (index >=0){
-//         let user = users[index];
-//         users.slice(index, 1);
-//         res.send(user);
-//         // users.splice(index, 1);
-//     }else{
-//         res.status(404)
-//         res.send({error: "not calid user id"})
-//     }
-// });
-
-// app.put('/api/users/:id', (req, res) => {
-//     let id = req.params.id;
-//     let userName = req.body.username;
-//     let pass = req.body.password;
-
-//     let index = -1;
-//     for (let user of users) {
-//         if (user.id === parseInt(id)){
-//             index = user.id -1;
-//         }
-//     }
-//     if (index>=0){
-//         let user = users[index];
-//         user.username=userName;
-//         user.password=pass;
-//         res.send(user);
-//     }else{
-//         res.status(404)
-//         res.send({error: "not calid user id"})
-//     }
-// });
 
 
 
