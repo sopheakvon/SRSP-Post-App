1bbc8c45df99b6fde9430a3a070f337fc201c9c5
saravey2021 <sara.vey@student.passerellesnumeriques.org>
2021-05-22 21:32:14 +0700

    girl

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,116 @@
-function displayComment (response){
-    let comments = response.data;
-    let removemessage = document.querySelectorAll('.alert');
-    
-    for(let message of removemessage){
-        
-        message.remove();
-    }
+function messageModal(user, mes, color, b, i, u) {
+    const sms = document.createElement("div");
+    sms.classList.add("alert");
+    sms.classList.add(color);
 
-    for(let comment of comments){
-        const sms = document.createElement('div');
-        // console.log(sms);
-        sms.classList.add('alert');
-        sms.classList.add('alert-info');
+    const userName = document.createElement("strong");
+    const messages = document.createElement("span");
 
-        const userName = document.createElement('strong');
-        const messages = document.createElement('span');
-
-        userName.textContent = comment.username + " : ";
-        messages.textContent = comment.message;
-
-        sms.appendChild(userName);
-        sms.appendChild(messages);
-        cardComment.appendChild(sms);
-    }
-}
-
-function sendMessage (){
-    
-    const messages = document.querySelector('#comment').value;
-    let messageBody = {
-        username: "Unknown", message: messages
+    if(b !== ""){
+        messages.classList.add(b);
     };
-    console.log(messageBody);
-    const url ="http://localhost:5000/comments";
-    axios
-    .post(url,messageBody)
-    .then(displayComment)
-}
+    if(i !== ""){
+        messages.classList.add(i);
+    };
+    if(u !== ""){
+        messages.classList.add(u);
+    };
+    
+    userName.textContent = user + " : ";
+    messages.textContent = mes;
+    
+    sms.appendChild(userName);
+    sms.appendChild(messages);
+    cardComment.appendChild(sms);
+};
 
-function loadMessage(){
-    const url ="http://localhost:5000/comments";
+function displayComment(response) {
+    let comments = response.data;
+    let removemessage = document.querySelectorAll(".alert");
+    for (let message of removemessage) {
+        message.remove();
+
+    };
+    for (let comment of comments) {
+        messageModal(comment.username, comment.message, comment.color, comment.b, comment.i, comment.u);
+    };
+};
+
+function sendMessage() {
+    const messages = document.querySelector("#comment").value;
+    let messageBody = {
+        username: "Unknown",
+        message: messages,
+        color: "alert-dark",
+        b: "",
+        i: "i",
+        u: ""
+    };
+    const url = "http://localhost:5000/comments";
     axios
-    .get(url)
-    .then(displayComment)
-}
-const btnSend = document.querySelector('button');
-btnSend.addEventListener('click',sendMessage);
-const cardComment = document.querySelector('.card-comment');
+    .post(url, messageBody)
+    .then(displayComment);
+
+    document.querySelector("#comment"). value = "";
+    disableButton();
+};
+
+let enableButton = () => {
+    btnSend.removeAttribute('disabled')
+};
+let disableButton = () => {
+    btnSend.setAttribute('disabled', '')
+};
+document.addEventListener('keyup', () => {
+    const messages = document.querySelector("#comment").value;
+    if (messages !=="") {
+        enableButton()
+        console.log(messages);
+    } else {
+        disableButton();
+    };
+});
+const btnSend = document.querySelector("#btnid");
+
+
+
+
+function loadMessage() {
+    const url = "http://localhost:5000/comments";
+    axios.get(url).then(displayComment);
+};
+
+function textstyles(e){
+    const bold = document.querySelector('#bolder');
+    const italic = document.querySelector('#italic');
+    const underLine = document.querySelector('#underline');
+    if(e.target.id === "bolder"){
+        bold.classList.toggle("activer");
+    };
+    if(e.target.id === "italic"){
+        italic.classList.toggle("activer");
+    };
+    if(e.target.id === "underline"){
+        underLine.classList.toggle("activer");
+    };
+
+
+    // if (e.target.id === "bolder" && e.target.className === "input-group-text activer") {
+    //     b = bolder.textContent.toLowerCase();
+    // }
+    // if (e.target.className === "input-group-text" && e.target.id === "bolder") {
+    //     b = "";
+    // }
+    // if (e.target.id === "italic" && e.target.className === "input-text-activer") {
+    //     i = italic.textContent.toLowerCase();
+    // }
+
+};
+
+
+btnSend.addEventListener("click", sendMessage);
+const cardComment = document.querySelector(".card-comment");
+const textStyle = document.querySelector(".text-style");
+textStyle.addEventListener("click", textstyles);
+
 loadMessage();
--- server.js
+++ server.js
@@ -1,35 +1,64 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
 
 app.listen(port, () => console.log("server starting ..."));
-
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-let comments  = [
-    {id: 1, username: "Sara", message: "Hello!! My name is Sara"},
-    {id: 2, username: "Sopheak", message: "Hello!! My name is Sopheak"},
-    {id: 2, username: "Rady", message: "Hello!! My name is Rady"},
+let comments = [
+  {
+    id: 1,
+    username: "Sara",
+    message: "Hello!! My name is Sara",
+    color: "alert-warning",
+    b: "",
+    i: "",
+    u: "u"
+  },
+  {
+    id: 2,
+    username: "Sopheak",
+    message: "Hello!! My name is Sopheak",
+    color: "alert-danger",
+    b: "",
+    i: "i",
+    u: ""
+    
+  },
+  {
+    id: 3,
+    username: "Rady",
+    message: "Hello!! My name is Rady",
+    color: "alert-success",
+    b: "b",
+    i: "",
+    u: ""
+  },
 ];
 
 // GET Method
-app.get('/comments', (req, res) =>{
-    res.send(comments)
-});
-app.post('/comments', (req, res) =>{
-    let user = req.body.username;
-    let sms = req.body.message;
-    let message = {
-        id: comments.length + 1,
-        username: user,
-        message: sms,
-    }
-    comments.push(message);
-    res.send(comments);
-    
+app.get("/comments", (req, res) => {
+  res.send(comments);
 });
 
-
-
+app.post("/comments", (req, res) => {
+  let user = req.body.username;
+  let sms = req.body.message;
+  let col = req.body.color;
+  let bol = req.body.b;
+  let ita = req.body.i;
+  let under = req.body.u;
+  let message = {
+    id: comments.length + 1,
+    username: user,
+    message: sms,
+    color: col,
+    b: bol,
+    i: ita,
+    u: under
+  };
+  comments.push(message);
+  res.send(comments);
+});
